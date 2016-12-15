890f83eec6212fc81a5fee6299436f5839a29bc4
Justin Voshell <me@justinvoshell.com>
2016-12-15 05:29:31 -0600

    Measures: inline time signatures, '*' measures, and min/maj aggregation

--- src/Measure.js
+++ src/Measure.js
@@ -6,8 +6,14 @@ class Measure {
   constructor() {
     this.raw = null
     this.beats = null
+    this.beatValue = null
+    this.beatsOverride = null
+    this.beatValueOverride = null
+    this.nMajor = 0 
+    this.nMinor = 0 
     this.tonic = null
     this.chords = null
+    this.isComplex = false
     this.isInvalid = false
     this.error = null
   }
@@ -16,18 +22,30 @@ class Measure {
     let result = new Measure()
     result.raw = rawString.trim()
 
+    if ( result.raw === '*' ) {
+      result.isComplex = true
+      return result
+    }
+
     if (! metre ) {
       result.isInvalid = true
       result.error = "No metre provided."
     }
     else {
-      let metreParts =  /([0-9]+)\/[0-9]+/.exec( metre )
+      let metreParts =  /([0-9]+)\/([0-9]+)/.exec( metre )
       if (! metreParts) {
         result.isInvalid = true
         result.error = "Cannot parse metre: " + metre
       }
 
-      result.beats = parseInt( metreParts[1] )
+      result.beats     = parseInt( metreParts[1] )
+      result.beatValue = parseInt( metreParts[2] )
+    }
+
+    let metreOverrideParts = /^\(([0-9]+)\/([0-9]+)\)/.exec( result.raw )
+    if ( metreOverrideParts ) {
+      result.beatsOverride     = parseInt( metreOverrideParts[1] )
+      result.beatValueOverride = parseInt( metreOverrideParts[2] )
     }
    
     if (! tonic ) {
@@ -42,9 +60,20 @@ class Measure {
       }
     }
 
+    // Parse chord strings in measure
+
     result.chords = []
 
-    let rawChords = result.raw.split(' ')
+    let rawChords = result.raw.split(' ') 
+
+    // Remove in-line time signature override from
+    // list of chords to parse
+
+    if ( result.beatsOverride )
+      rawChords = _.drop( rawChords )
+
+    // Perform expansion of '.' chords
+
     let lastChord = null
     for (var rawChord of rawChords) {
       if (rawChord === '.') 
@@ -54,19 +83,43 @@ class Measure {
       result.chords.push( Chord.fromString( rawChord ) )
     } 
 
-    if ( result.beats && result.beats > result.chords.length ) {
-      if ( result.beats % result.chords.length === 0 ) {
-        result.chords = _.concat( 
-                          _.flatten(
-                            _.map( result.chords, 
-                                   chord => _.times( result.beats / result.chords.length, 
-                                                     _.wrap( chord, _.cloneDeep ) ) ) ) )
-      }
-      else {
-        result.isInvalid = true
-        result.error = "Could not expand " + result.chords.length + " chords to " + result.beats + " beats."
-      }
+    // Attempt to fill specified number of beats
+    // using provided chord list
+
+    let effectiveBeats = result.beatsOverride 
+                         ? result.beatsOverride : result.beats
+
+    if ( effectiveBeats % result.chords.length === 0 ) {
+      result.chords = 
+        _.concat( 
+          _.flatten(
+            _.map( result.chords, 
+                   chord => _.times( effectiveBeats / result.chords.length, 
+                                     _.wrap( chord, _.cloneDeep ) ) ) ) )
     }
+
+    // Record error if beat count and chord list length 
+    // are mismatched.
+
+    else {
+      result.isInvalid = true
+      result.error = "Could not expand " + result.chords.length + " chords to " + effectiveBeats + " beats."
+    }
+
+    // Count major and minor chords
+
+    let minmajCount =
+      _.reduce( result.chords,
+                function( totals, nextChord ) { 
+                  return {
+                    nMajor: ( totals.nMajor + ( ( nextChord.isMajor ) ? 1 : 0 ) ),
+                    nMinor: ( totals.nMinor + ( ( nextChord.isMinor ) ? 1 : 0 ) )
+                  }
+                },
+                { nMajor: 0, nMinor: 0 } )
+
+    result.nMajor = minmajCount.nMajor
+    result.nMinor = minmajCount.nMinor
 
     return result
   }
--- test/Measure.js
+++ test/Measure.js
@@ -25,12 +25,15 @@ describe('Measure', function() {
       let measureText = 'D:maj . G:maj/5 D:maj'
       let measure = Measure.fromString(measureText, '4/4')
       measure.should.have.property('beats', 4)
-      
+      measure.should.have.property('beatValue', 4)
+
       measure = Measure.fromString(measureText, '3/4')
       measure.should.have.property('beats', 3) 
+      measure.should.have.property('beatValue', 4)
 
       measure = Measure.fromString(measureText, '6/8')
       measure.should.have.property('beats', 6) 
+      measure.should.have.property('beatValue', 8)
     })
 
     it('Creates chord sequence from provided input', function() {
@@ -124,5 +127,64 @@ describe('Measure', function() {
     })
   })
 
+  describe('Statistics', function() {
+    it('provides count of major and minor chords in a measure', function() {
+      let measure = Measure.fromString( 'D:maj', '4/4', 'D' )
+      measure.should.have.property('nMajor', 4, JSON.stringify( measure ))
+
+      measure = Measure.fromString( 'C:hdim7 E:5 G:1(9,11,b3)', '6/8', 'C' )
+      measure.should.have.property('nMajor', 2, JSON.stringify( measure ))
+      measure.should.have.property('nMinor', 2, JSON.stringify( measure ))
+    })
+  })
+
+  describe('Special notation', function() {
+    it('handles "*" notation for musically complex measures', function() {
+      
+      // In this case, the measure considered to be too complex for 
+      // assigning chords to beats.  Chord content, time signature content,
+      // etc. is left null. 'isComplex' is marked as true.
+
+      let measure = Measure.fromString( '*' )
+
+      measure.should.containSubset({
+        raw: '*',
+        beats: null,
+        beatsOverride: null,
+        beatValue: null,
+        beatValueOverride: null,
+        tonic: null,
+        chords: null,
+        nMajor: 0,
+        nMinor: 0,
+        isInvalid: false,
+        error: null,
+        isComplex: true
+      })
+    })
+
+    it('handles in-line time signature changes', function() {
+      
+      // In this case, we ignore the time signature provided as the 2nd arg
+      // in favor of the embedded signature.  This removes the need for
+      // routines to understand and lookout for embedded signatures.
+
+      let measure = Measure.fromString( '(6/4) C:maj A:maj D:maj', '4/4', 'C' )
+      
+      measure.should.containSubset({ 
+        beats: 4,
+        beatValue: 4,
+        beatsOverride: 6,
+        beatValueOverride: 4,
+        chords: [
+          { raw: 'C:maj' },
+          { raw: 'C:maj' },
+          { raw: 'A:maj' },
+          { raw: 'A:maj' },
+          { raw: 'D:maj' },
+          { raw: 'D:maj' }]
+      })
+    })
+  })
 })
 
