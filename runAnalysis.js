9d8ac43fe1448628a038b30876d9fe06158a9ed9
Justin Voshell <justinvoshell@Justins-MBP-2.fios-router.home>
2018-12-07 10:37:52 -0500

    Update for Scott.

--- runAnalysis.js
+++ runAnalysis.js
@@ -1,109 +1,155 @@
-const _ = require( 'lodash' )
-const fs = require( 'fs' )
-const MetadataReader = require( './src/MetadataReader.js' )
-const SongSet = require( './src/SongSet' )
-const SongBillboardReader = require( './src/SongBillboardReader' )
-const SongExcelWriter= require( './src/SongExcelWriter' )
+const fs = require("fs");
+const MetadataReader = require("./src/MetadataReader.js");
+const SongSet = require("./src/SongSet");
+const SongBillboardReader = require("./src/SongBillboardReader");
+const SongExcelWriter = require("./src/SongExcelWriter");
+const rimraf = require("rimraf");
 
+/*
+    Prepare output area
+ */
 
-const parseInputFiles = async function ( inputFiles ) {
-  let parsedInputFiles = []
-  for ( var idx = 0; idx < inputFiles.length; ++idx ) {
-    let inputFile = inputFiles[idx]
-    console.log(`Parsing: ${inputFile}`)
-    parsedInputFiles.push( await SongBillboardReader.parseFile( inputFiles[idx] ) ) 
-  }
-  return parsedInputFiles;
+const OUTPUT_DIRECTORY = './output';
+
+// Remove any existing output
+rimraf.sync(OUTPUT_DIRECTORY);
+
+// Create a clean output directory
+fs.mkdirSync(OUTPUT_DIRECTORY);
+
+/*
+    Load song files
+*/
+
+// There's a parent directory for the entire dataset
+const DATASET_DIRECTORY = './source_data/McGill-Billboard';
+
+// There are sub-directories of the parent for each song.
+const song_directories = fs.readdirSync(DATASET_DIRECTORY).filter(
+    sub_directory => fs.statSync(DATASET_DIRECTORY + '/' + sub_directory).isDirectory()
+);
+
+// In the McGill Billboard dataset, each song's data is help in a file called 'salami_chords.txt'.
+const SONG_DATA_FILENAME = 'salami_chords.txt';
+
+// Inside of each song's sub-directory is the 'salami' file with the song's data
+const allSongFiles = song_directories.map(dir => `${DATASET_DIRECTORY}/${dir}/${SONG_DATA_FILENAME}`);
+
+/*
+    Load song metadata
+ */
+
+const SONG_METADATA = MetadataReader.fromXlsxFile('./source_data/six_time_bins_1.xlsx', 'Sheet1');
+
+/*
+    Run analyses
+ */
+
+// To run analyses, parse all of the song files, then call 'analysisRun' with the desired filters and reports
+
+parseSongFiles(allSongFiles).then(
+    parsedSongFiles => analyzeByTimeBin(parsedSongFiles)
+    // parsedSongFiles => analyzeBySongYear(parsedSongFiles)
+);
+
+/*
+    Analyses
+ */
+
+function analyzeByTimeBin(allSongFiles) {
+    const startBin = 1;
+    const endBin = 6;
+
+    let bin = endBin;
+    for (let idx = 0; idx <= endBin - 1; ++idx) {
+        bin = endBin - idx;
+
+        analysisRun(`Full time bin ${bin}`, allSongFiles, {
+            filters: [
+                song => song.tonality() === 'major',
+                song => parseInt(song.metadata.time_bin) === bin
+            ],
+            reports: [
+                {granularity: 'SECTION', filepath: `output/${bin}-section.xlsx`},
+                {granularity: 'SONG', filepath: `output/${bin}-song.xlsx`}
+            ]
+        });
+
+        analysisRun(`Only bins ${startBin}-${bin}`, allSongFiles, {
+            filters: [
+                song => song.tonality() === 'major',
+                song => parseInt(song.metadata.time_bin) >= startBin,
+                song => parseInt(song.metadata.time_bin) <= bin
+            ],
+            reports: [
+                {granularity: 'SECTION', filepath: `output/${bin}-${startBin}-section.xlsx`},
+                {granularity: 'SONG', filepath: `output/${bin}-${startBin}-song.xlsx`}
+            ]
+        })
+    }
+
+    console.log(`Analysis run complete.`);
 }
 
-const analysisRun = async function( parsedInputFiles, analysisParams ) {
+function analyzeBySongYear(allSongFiles) {
+    const startYear = 1958;
+    const endYear = 1991;
 
-  let songSet = 
-    new SongSet( additionalInputMetadata, analysisParams.filters )
-
-  for ( var idx = 0; idx < parsedInputFiles.length; ++idx ) {
-    let parsedInputFile = parsedInputFiles[idx]
-    songSet.add( parsedInputFile )
-  }
-
-  for ( var idx2 = 0; idx2 < analysisParams.reports.length; ++idx2 ) {
-    let report = analysisParams.reports[idx2];
-    console.log(`Writing: ${report.filepath}`)
-    SongExcelWriter.writeSongSet( songSet, report.granularity, report.filepath )
-  }
-
-  return true
+    let year = endYear;
+    for (let idx = 0; idx <= endYear - startYear; ++idx) {
+        year = endYear - idx;
+        analysisRun(allSongFiles, {
+            filters: [
+                song => song.tonality() === 'major',
+                song => parseInt(song.metadata.year) === year
+            ],
+            reports: [
+                {granularity: 'SECTION', filepath: `output/${year}-section.xlsx`},
+                {granularity: 'SONG', filepath: `output/${year}-song.xlsx`}
+            ]
+        });
+        analysisRun(allSongFiles, {
+            filters: [
+                song => song.tonality() === 'major',
+                song => parseInt(song.metadata.year) >= startYear,
+                song => parseInt(song.metadata.year) <= year
+            ],
+            reports: [
+                {granularity: 'SECTION', filepath: `output/${year}-${startYear}-section.xlsx`},
+                {granularity: 'SONG', filepath: `output/${year}-${startYear}-song.xlsx`}
+            ]
+        });
+    }
 }
 
-const getDirs = 
-  p => fs.readdirSync( p ).filter( 
-      f => fs.statSync( p + '/' + f ).isDirectory() )
+/*
+    Supporting functions
+ */
 
-const filepaths = getDirs( './source_data/McGill-Billboard' )
-const inputFiles = _.map( filepaths, n => `./source_data/McGill-Billboard/${n}/salami_chords.txt` )
-// const additionalInputMetadata = MetadataReader.fromXlsxFile('./source_data/mappings.xlsx', 'Sheet4')
-const additionalInputMetadata = MetadataReader.fromXlsxFile('./source_data/six_time_bins_1.xlsx', 'Sheet1')
+async function parseInputFile(inputFile) {
+    console.log(`Parsing: ${inputFile}`);
+    return await SongBillboardReader.parseFile(inputFile);
+}
 
-/* parseInputFiles( inputFiles ).then( ( parsedInputFiles ) => {
-  const startYear = 1958
-  const endYear = 1991
+async function parseSongFiles(inputFiles) {
+    const parsedInputFiles = [];
 
-  let year = endYear
-  for ( var idx = 0; idx <= endYear - startYear; ++idx ) { 
-    year = endYear - idx
-    analysisRun( parsedInputFiles, {
-      filters: [
-        song => song.tonality() === 'major',
-        song => parseInt(song.metadata.year) === year 
-      ],
-      reports: [
-        { granularity: 'SECTION', filepath: `output/${year}-section.xlsx`},
-        { granularity: 'SONG', filepath: `output/${year}-song.xlsx`}
-      ]
-    })
-    analysisRun( parsedInputFiles, {
-      filters: [
-        song => song.tonality() === 'major',
-        song => parseInt(song.metadata.year) >= startYear,
-        song => parseInt(song.metadata.year) <= year
-      ],
-      reports: [
-        { granularity: 'SECTION', filepath: `output/${year}-${startYear}-section.xlsx` },
-        { granularity: 'SONG', filepath: `output/${year}-${startYear}-song.xlsx` }
-      ]
-    })
-  }
-})*/
+    for (const inputFile of inputFiles) {
+        parsedInputFiles.push(await parseInputFile(inputFile));
+    }
+    return parsedInputFiles;
+}
 
-parseInputFiles( inputFiles ).then( ( parsedInputFiles ) => {
-  const startBin = 1 
-  const endBin = 6 
+function analysisRun(name, parsedInputFiles, analysisParams) {
+    console.log(`Starting analysis run: ${name}.`);
 
-  let bin = endBin
-  for ( var idx = 0; idx <= endBin - 1; ++idx ) { 
-    bin = endBin - idx
-    analysisRun( parsedInputFiles, {
-      filters: [
-        song => song.tonality() === 'major',
-        song => parseInt(song.metadata.time_bin) === bin 
-      ],
-      reports: [
-        { granularity: 'SECTION', filepath: `output/${bin}-section.xlsx`},
-        { granularity: 'SONG', filepath: `output/${bin}-song.xlsx`}
-      ]
-    })
-    analysisRun( parsedInputFiles, {
-      filters: [
-        song => song.tonality() === 'major',
-        song => parseInt(song.metadata.time_bin) >= startBin,
-        song => parseInt(song.metadata.time_bin) <= bin 
-      ],
-      reports: [
-        { granularity: 'SECTION', filepath: `output/${bin}-${startBin}-section.xlsx` },
-        { granularity: 'SONG', filepath: `output/${bin}-${startBin}-song.xlsx` }
-      ]
-    })
-  }
-})
+    const songSet = new SongSet(SONG_METADATA, analysisParams.filters);
+    parsedInputFiles.forEach(songSet.add.bind(songSet));
+    analysisParams.reports.forEach(report => {
+        console.log(`Writing: ${report.filepath}`);
+        SongExcelWriter.writeSongSet(songSet, report.granularity, report.filepath);
+    });
 
-
+    console.log(`Analysis run: ${name} complete.`);
+    return true
+}
